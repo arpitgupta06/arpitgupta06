ae760975e874aa1532d067195719089d47d45f05
Arpit Gupta <cloudify4@gmail.com>
2026-01-05 08:45:29 +0000

    changes in date_dimension table

--- models/example/date_dimension.sql
+++ models/example/date_dimension.sql
@@ -2,7 +2,12 @@ with cte as (
     select
     date(to_timestamp(started_at)) as started_at,
     hour(to_timestamp(started_at)) as hour_started_at,
-    dayofweek(to_timestamp(started_at)) days_started_at
+    dayname(to_timestamp(started_at)) days_started_at,
+    case
+        when dayname(to_timestamp(started_at)) in ('Sat', 'Sun')
+        then 'Weekend'
+        else 'Businessday'
+    end as day_type
     from {{ source('demo', 'ny_bike') }}
 )
 
